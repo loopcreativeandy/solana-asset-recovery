b6413e18bf9e8babe5479d82fab087d0fad03219
Solandy <solandy.consulting@gmail.com>
2024-02-18 20:54:11 +0100

    change title

--- web/components/dashboard/dashboard-feature.tsx
+++ web/components/dashboard/dashboard-feature.tsx
@@ -16,22 +16,11 @@ const links: { label: string; href: string }[] = [
 export default function DashboardFeature() {
   return (
     <div>
-      <AppHero title="gm" subtitle="Say hi to your new Solana dApp." />
+      <AppHero title="gm" subtitle="Solandys recovery tool." />
       <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div className="space-y-2">
-          <p>Here are some helpful links to get you started.</p>
-          {links.map((link, index) => (
-            <div key={index}>
-              <a
-                href={link.href}
-                className="link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {link.label}
-              </a>
-            </div>
-          ))}
+          <p>Use this tool to recover tokens from wallets that are owned by other programs.</p>
+          
         </div>
       </div>
     </div>
