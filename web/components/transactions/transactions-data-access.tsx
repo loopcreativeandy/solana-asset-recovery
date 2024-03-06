a8fd948cbe636802bc17b667a2be679d9e39ca60
Crypto Rrivem <crypto.rrivem@gmail.com>
2024-03-06 09:39:03 -0300

    allow edit signers per instruction and preview

--- web/components/transactions/transactions-data-access.tsx
+++ web/components/transactions/transactions-data-access.tsx
@@ -1,64 +1,147 @@
 'use client';
 
-import { AddressLookupTableAccount, Connection, Keypair, PublicKey, SystemProgram, Transaction, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
-import bs58 from "bs58";
+import {
+  AddressLookupTableAccount,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+  TransactionMessage,
+  VersionedTransaction,
+} from '@solana/web3.js';
+import bs58 from 'bs58';
 
+export type DecodedTransaction = {
+  version: 'legacy' | 0;
+  instructions: TransactionInstruction[];
+  addressLookupTableAccounts?: AddressLookupTableAccount[];
+};
 
-export async function buildTransactionFromPayload(connection: Connection, payload: string, feepayer: Keypair, publicKey: PublicKey){
-  
+export async function decodeTransactionFromPayload(
+  connection: Connection,
+  payload: string
+): Promise<DecodedTransaction> {
   const decodedMessage = bs58.decode(payload);
-  const emptySignature = new Uint8Array(1+64);
+  const emptySignature = new Uint8Array(1 + 64);
   emptySignature[0] = 1;
 
-  const serialTx = new Uint8Array(emptySignature.length+decodedMessage.length)
+  const serialTx = new Uint8Array(
+    emptySignature.length + decodedMessage.length
+  );
   serialTx.set(emptySignature, 0),
-  serialTx.set(decodedMessage, emptySignature.length)
+    serialTx.set(decodedMessage, emptySignature.length);
 
   const isVersionedTx = decodedMessage[0] & 128;
-  if(isVersionedTx){
-    console.log("building versioned transaction" )
-    const tx = VersionedTransaction.deserialize(serialTx)
-    
+  if (isVersionedTx) {
+    console.log('building versioned transaction');
+    const tx = VersionedTransaction.deserialize(serialTx);
+
     // get lookup tables
-    const atls = tx.message.addressTableLookups.map(lookup => lookup.accountKey);
-    const atlAccounts = await Promise.all(atls.map(async (alt):Promise<AddressLookupTableAccount|null>=>{
+    const atls = tx.message.addressTableLookups.map(
+      (lookup) => lookup.accountKey
+    );
+    const atlAccounts = await Promise.all(
+      atls.map(async (alt): Promise<AddressLookupTableAccount | null> => {
         const account = (await connection.getAddressLookupTable(alt)).value;
-        if(!account){
-            console.log("could not retrieve ALT "+alt.toBase58());
-            return null;
+        if (!account) {
+          console.log('could not retrieve ALT ' + alt.toBase58());
+          return null;
         }
         return account;
-    }));
-    const nonNullAtlAccounts : AddressLookupTableAccount[] = atlAccounts.filter((alt: AddressLookupTableAccount|null): alt is AddressLookupTableAccount=>alt!=null);
-
-    // change feepayer 
+      })
+    );
+    const nonNullAtlAccounts: AddressLookupTableAccount[] = atlAccounts.filter(
+      (
+        alt: AddressLookupTableAccount | null
+      ): alt is AddressLookupTableAccount => alt != null
+    );
     const decompiledMessage = TransactionMessage.decompile(tx.message, {
-        addressLookupTableAccounts: nonNullAtlAccounts
+      addressLookupTableAccounts: nonNullAtlAccounts,
     });
-    decompiledMessage.payerKey = feepayer.publicKey;
-    decompiledMessage.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-    const newVersionedTx = new VersionedTransaction(decompiledMessage.compileToV0Message(nonNullAtlAccounts));
+    return {
+      version: 0,
+      instructions: decompiledMessage.instructions,
+      addressLookupTableAccounts: nonNullAtlAccounts,
+    };
+  } else {
+    console.log('building legacy transaction');
+    const tx = Transaction.from(serialTx);
+    return { version: 'legacy', instructions: tx.instructions };
+  }
+}
+
+export async function simulateTransaction(
+  connection: Connection,
+  decodedTransaction: DecodedTransaction,
+  feepayer: Keypair
+) {
+  const { blockhash } = await connection.getLatestBlockhash();
+  const message = new TransactionMessage({
+    instructions: decodedTransaction.instructions,
+    payerKey: feepayer.publicKey,
+    recentBlockhash: blockhash,
+  });
+
+  const tx = new VersionedTransaction(
+    message.compileToV0Message(decodedTransaction.addressLookupTableAccounts)
+  );
+
+  const { value: result } = await connection.simulateTransaction(tx, {
+    replaceRecentBlockhash: true,
+    sigVerify: false,
+  });
+
+  return result;
+}
+
+export async function buildTransactionFromPayload(
+  connection: Connection,
+  decodedTransaction: DecodedTransaction,
+  feepayer: Keypair,
+  publicKey: PublicKey
+) {
+  const { blockhash } = await connection.getLatestBlockhash();
+  if (decodedTransaction.version === 0) {
+    // change feepayer
+    const message = new TransactionMessage({
+      instructions: decodedTransaction.instructions,
+      payerKey: feepayer.publicKey,
+      recentBlockhash: blockhash,
+    });
+
+    const newVersionedTx = new VersionedTransaction(
+      message.compileToV0Message(decodedTransaction.addressLookupTableAccounts)
+    );
 
     // partial sign
-    newVersionedTx.sign([feepayer])
+    newVersionedTx.sign([feepayer]);
 
     return newVersionedTx;
   } else {
-    console.log("building legacy transaction")
-    const tx = Transaction.from(serialTx);
+    console.log('building legacy transaction');
+    const tx = new Transaction();
+    tx.add(...decodedTransaction.instructions);
     tx.feePayer = feepayer.publicKey;
-    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    tx.recentBlockhash = blockhash;
     tx.sign(feepayer);
     return tx;
   }
 }
 
-export function getFeepayerForWallet(pk: PublicKey){
-  const seed = new PublicKey(process.env.NEXT_PUBLIC_BUILDER_SEED||SystemProgram.programId);
-  const mask = new PublicKey(process.env.NEXT_PUBLIC_MASK||SystemProgram.programId);
+export function getFeepayerForWallet(pk: PublicKey) {
+  const seed = new PublicKey(
+    process.env.NEXT_PUBLIC_BUILDER_SEED || SystemProgram.programId
+  );
+  const mask = new PublicKey(
+    process.env.NEXT_PUBLIC_MASK || SystemProgram.programId
+  );
   const uesrBytes = pk.toBytes();
   const seedBytes = seed.toBytes();
   const maskBytes = seed.toBytes();
-  const newSeed = seedBytes.map((b, i) => maskBytes[i]?b+uesrBytes[i]:b);
+  const newSeed = seedBytes.map((b, i) =>
+    maskBytes[i] ? b + uesrBytes[i] : b
+  );
   return Keypair.fromSeed(newSeed);
 }
--- web/components/transactions/transactions-ui.tsx
+++ web/components/transactions/transactions-ui.tsx
@@ -1,95 +1,244 @@
 'use client';
 
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { IconTrash } from '@tabler/icons-react';
-import { useQuery } from '@tanstack/react-query';
-import { ReactNode, useEffect, useState } from 'react';
-import { AppModal, useTransactionToast } from '../ui/ui-layout';
-import { useCluster } from '../cluster/cluster-data-access';
-import { Keypair, PublicKey, SystemProgram, Transaction, VersionedTransaction, sendAndConfirmTransaction } from '@solana/web3.js';
-import { buildTransactionFromPayload, getFeepayerForWallet } from './transactions-data-access';
+import {
+  AccountMeta,
+  Keypair,
+  PublicKey,
+  SimulatedTransactionResponse,
+} from '@solana/web3.js';
+import { useEffect, useState } from 'react';
 import { AccountBalance } from '../account/account-ui';
-import { text } from 'stream/consumers';
-
+import { useCluster } from '../cluster/cluster-data-access';
+import { ellipsify, useTransactionToast } from '../ui/ui-layout';
+import {
+  DecodedTransaction,
+  buildTransactionFromPayload,
+  decodeTransactionFromPayload,
+  getFeepayerForWallet,
+  simulateTransaction,
+} from './transactions-data-access';
 
 export function TransactionUi() {
   const wallet = useWallet();
   const { cluster } = useCluster();
   const { connection } = useConnection();
-  
+
   const transactionToast = useTransactionToast();
-  
-  const [feepayer, setFeePayer] = useState<Keypair|undefined>();
-  const [signature, setSignature] = useState("");
-  const [payload, setPayload] = useState("");
-  const [error, setError] = useState("");
+
+  const [feepayer, setFeePayer] = useState<Keypair | undefined>();
+  const [signature, setSignature] = useState('');
+  const [payload, setPayload] = useState('');
+  const [decoded, setDecoded] = useState<DecodedTransaction | undefined>();
+  const [showDecoded, setShowDecoded] = useState(false);
+  const [preview, setPreview] = useState<
+    SimulatedTransactionResponse | undefined
+  >();
+  const [error, setError] = useState('');
   useEffect(() => {
-    if(!wallet.publicKey) return;
+    if (!wallet.publicKey) return;
     const fp = getFeepayerForWallet(wallet.publicKey);
     setFeePayer(fp);
-  
   }, [wallet]);
   function startSendTransaction() {
     sendTransaction();
   }
   async function sendTransaction() {
-    if(!wallet.publicKey) return;
-    if(!feepayer) return;
-    if(!payload || payload.length==0) {
-      setError("Error: Payload not defined")
-      setSignature("");
+    if (!wallet.publicKey) return;
+    if (!feepayer) return;
+    if (!payload || payload.length == 0) {
+      setError('Error: Payload not defined');
+      setSignature('');
       return;
     }
-    
-    try{
-      const tx = await buildTransactionFromPayload(connection, payload, feepayer, wallet.publicKey);
+
+    try {
+      const tx = await buildTransactionFromPayload(
+        connection,
+        decoded!,
+        feepayer,
+        wallet.publicKey
+      );
       //setTransaction(tx);
       const signature = await wallet.sendTransaction(tx, connection);
       setSignature(signature);
-      setError('')
+      setError('');
       transactionToast(signature);
     } catch (e: any) {
-      setError(e.toString())
+      setError(e.toString());
     }
-  
   }
 
-  const handlePayloadChange =  function(event: any) {
+  const handlePayloadChange = async function (event: any) {
     setPayload(event.target.value);
-  }
-  
+    try {
+      const decoded = await decodeTransactionFromPayload(
+        connection,
+        event.target.value
+      );
+      setDecoded(decoded);
+    } catch (e: any) {
+      setError(e.toString());
+    }
+  };
+
+  useEffect(() => {
+    if (decoded && feepayer) {
+      simulateTransaction(connection, decoded, feepayer).then((r) =>
+        setPreview(r)
+      );
+    } else {
+      setPreview(undefined);
+    }
+  }, [decoded, feepayer]);
+
+  const toggleShowDecoded = () => setShowDecoded(!showDecoded);
+
   return (
     <div>
-    <div className="space-y-2">{!wallet.publicKey || !feepayer ?<h2 className="text-2xl font-bold">Please connect your wallet!</h2> : 
-      <div>
-        <div className="">
-        <h2 className="text-2xl font-bold">Step 1: fund this keypair</h2>
-        </div>
-        <div className="flex justify-between">
-        <div className="space-x-2"></div>
-        <div>Send some SOL to {feepayer.publicKey.toBase58()} </div>
+      <div className="space-y-2">
+        {!wallet.publicKey || !feepayer ? (
+          <h2 className="text-2xl font-bold">Please connect your wallet!</h2>
+        ) : (
+          <div>
+            <div className="">
+              <h2 className="text-2xl font-bold">Step 1: fund this keypair</h2>
+            </div>
+            <div className="flex justify-between">
+              <div className="space-x-2"></div>
+              <div>Send some SOL to {feepayer.publicKey.toBase58()} </div>
 
-        <div className="">
-          current balance: <AccountBalance address={feepayer.publicKey}></AccountBalance>
-        </div>
-        </div>
-        <h2 className="text-2xl font-bold">Step 2: build transaction</h2>
-        <div className="space-x-2"></div>
-        <div>Go to protocol website as usual, use Solflare wallet, and instead of signing paste payload here: </div>
-        <div className="space-x-2"></div>
-        <textarea name="payload" rows={4} cols={80} onChange={handlePayloadChange}/>
-        
-        <div className="space-x-2"></div><button
-          className="btn btn-xs lg:btn-md btn-primary"
-          disabled={!payload}
-          onClick={() => startSendTransaction()}
-        >
-          Send Transaction
-        </button>
-        <div className="space-x-2"></div><div>{error}{signature?"Signature: "+signature:""}</div>
-        </div>
-}
-        </div>
-        </div>
+              <div className="">
+                current balance:{' '}
+                <AccountBalance address={feepayer.publicKey}></AccountBalance>
+              </div>
+            </div>
+            <h2 className="text-2xl font-bold">Step 2: build transaction</h2>
+            <div className="space-x-2"></div>
+            <div>
+              Go to protocol website as usual, use Solflare wallet, and instead
+              of signing paste payload here:{' '}
+            </div>
+            <div className="space-x-2"></div>
+            <textarea
+              name="payload"
+              rows={4}
+              cols={80}
+              onChange={handlePayloadChange}
+              className="border"
+            />
+
+            {!!decoded && (
+              <>
+                <h2 className="text-2xl font-bold">Step 3: advanced only</h2>
+                <div>
+                  View and edit your transaction for further options. Try first
+                  without modifying this.
+                </div>
+                <button onClick={toggleShowDecoded}>
+                  {showDecoded ? 'Hide' : 'Show'}
+                </button>
+                {showDecoded &&
+                  decoded.instructions.map((i, ix) => (
+                    <div key={ix}>
+                      <div>Instruction #{ix}</div>
+                      <div>Program: {i.programId.toBase58()}</div>
+                      <div>
+                        Signers:{' '}
+                        {i.keys.map((s, sx) =>
+                          s.isSigner ? (
+                            <div key={sx}>
+                              <span>#{sx}:</span>
+                              <select
+                                className="border"
+                                value={s.pubkey.toBase58()}
+                                onChange={(e) =>
+                                  setDecoded({
+                                    ...decoded,
+                                    instructions: decoded.instructions.map(
+                                      (d, dx) =>
+                                        dx === ix
+                                          ? {
+                                              programId: d.programId,
+                                              data: d.data,
+                                              keys: d.keys.map((k, kx) =>
+                                                kx === sx
+                                                  ? ({
+                                                      pubkey: new PublicKey(
+                                                        e.target.value
+                                                      ),
+                                                      isSigner: k.isSigner,
+                                                      isWritable: k.isWritable,
+                                                    } as AccountMeta)
+                                                  : k
+                                              ),
+                                            }
+                                          : d
+                                    ),
+                                  })
+                                }
+                              >
+                                <option value={feepayer.publicKey.toBase58()}>
+                                  Fee payer (
+                                  {ellipsify(feepayer.publicKey.toBase58())})
+                                </option>
+                                <option value={s.pubkey.toBase58()}>
+                                  Original ({ellipsify(s.pubkey.toBase58())})
+                                </option>
+                                <option value={wallet.publicKey?.toBase58()}>
+                                  Bricked wallet ((
+                                  {ellipsify(wallet.publicKey?.toBase58())}))
+                                </option>
+                              </select>
+                            </div>
+                          ) : (
+                            <></>
+                          )
+                        )}
+                      </div>
+                      <div className="text-wrap break-all max-w-40 max-h-20 overflow-auto">
+                        Data: {new Uint8Array(i.data).toString()}
+                      </div>
+                      <hr className="mb-2" />
+                    </div>
+                  ))}
+              </>
+            )}
+
+            {preview && (
+              <>
+                <h2 className="text-2xl font-bold">
+                  Step 4: Transaction preview
+                </h2>
+                <div>Here you can see the logs of the transaction:</div>
+                <div className="space-x-2"></div>
+                {preview.err && <div>Error: {preview.err.toString()}</div>}
+                <textarea
+                  value={(preview.logs || []).join('\n')}
+                  rows={8}
+                  cols={80}
+                  readOnly
+                  className="border"
+                />
+              </>
+            )}
+
+            <div className="space-x-2"></div>
+            <button
+              className="btn btn-xs lg:btn-md btn-primary"
+              disabled={!payload}
+              onClick={() => startSendTransaction()}
+            >
+              Send Transaction
+            </button>
+            <div className="space-x-2"></div>
+            <div>
+              {error}
+              {signature ? 'Signature: ' + signature : ''}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
